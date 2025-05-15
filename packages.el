e43d9bc01a98ff7e0c9dcc313c8ec9dd650160d4
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-05-15 10:00:24 -0700

    refactor: re-add former package.el

--- packages.el
+++ packages.el
@@ -1,8 +1,10 @@
+
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
@@ -47,3 +49,43 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+;;
+(package! emacs-dropbox
+  :recipe (:host github :repo "lorniu/emacs-dropbox" :files ("*.el")))
+
+
+;; lsp tailwindcss
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+
+;; org-roam
+(package! org-roam
+  :recipe (:host github :repo "org-roam/org-roam" :branch "main"))
+(package! citar-org-roam)
+(package! org-roam-ui)
+(package! sqlite3)
+(package! pinentry)
+(package! gptel)
+(package! evil-visual-mark-mode)
+(package! solidity-mode)
+(package! solidity-flycheck)
+(package! company-solidity)
+(package! move-text)
+(package! rainbow-mode)
+(package! apheleia)
+(package! dired-narrow)
+(package! centered-cursor-mode)
+(package! treesit-auto)
+(package! exec-path-from-shell)
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+(package! prettier)
+(package! svg-tag-mode)
+(package! nvm)
+(package! aider :recipe (:host github :repo "tninja/aider.el" :files ("aider.el" "aider-core.el" "aider-file.el" "aider-code-change.el" "aider-discussion.el" "aider-prompt-mode.el" "aider-doom.el")))
+(package! solaire-mode)
+(package! pdf-tools)
+(package! org-download)
+(package! url)
+(package! aidermacs)
+(package! linear
+  :recipe (:host github :repo "anegg0/linear-emacs" :files ("*.el")))
