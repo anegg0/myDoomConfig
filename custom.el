e991248a166f7b7c5058aae605a276eee9359466
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-07-14 14:56:53 -0700

    update custom.el with new keybindings

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc"
+   '("4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e"
+     "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc"
      "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
  '(ignored-local-variable-values '((flycheck-disabled-checkers proselint textlint)))
  '(package-selected-packages '(async glab org-preview-html undo-fu undo-fu-session))
